d1e29910389c172c8aedd89920f7be3062591798
michaelzhao <michaelpublic@qq.com>
2023-04-26 13:55:14 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,10 @@
 
 env >> /etc/environment
 
-echo "${CRON}" > /var/spool/cron/crontabs/$(whoami)
+FILE="/var/spool/cron/crontabs/$(whoami)"
+
+echo "${JOB}" > $FILE
+
+for i in $(seq 0 9); do eval echo \"\${JOB$i}\" >> /var/spool/cron/crontabs/$(whoami); done
 
 exec "$@"
